a409175786cf92a242e4cf7b8352d6197e24d17f
Simran53999 <simran.thakur.5319@gmail.com>
2021-01-19 05:01:48 +0530

    Final files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom"; //for rendering
+
+import "./App.css";
+
 
 function App() {
+  /* eslint no-eval: 0 */
+
+  const [input, setInput] = useState("");
+  const calcBtns = [];
+  [7, 8, 9, 4, 5, 6, 1, 2, 3, 0,"00", "."].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => {
+          setInput(input + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {" "}
+        {item}
+      </button>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="wrapper">
+      {" "}
+      <div className="show-input">{input}</div>
+      <div className="digits flex">{calcBtns}</div>
+      <div className="modifiers subgrid">
+        {/* clear button */}
+
+        <button onClick={() => setInput(input.substr(0, input.length - 1))}>
+          Clear
+        </button>
+
+        {/* clear all */}
+        <button onClick={() => setInput("")} value="">
+          AC
+        </button>
+        <button onClick={(e) => setInput(input + e.target.value)} value="%">
+         %
+        </button>
+      </div>
+      <div className="operations subgrid">
+        {/* add button */}
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="/">
+          {" "}
+          ÷
+        </button>
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="*">
+          {" "}
+          *
+        </button>
+
+                {/* minus btn */}
+        <button onClick={(e) => setInput(input + e.target.value)} value="-">
+          {" "}
+          -{" "}
+        </button>
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="+">
+          +
+        </button>
+
+
+        {/* "=" btn */}
+        <button
+          onClick={(e) => {
+            try {
+              setInput(
+                String(eval(input)).length > 3 &&
+                  String(eval(input)).includes(".")
+                  ? String(eval(input).toFixed(4))
+                  : String(eval(input))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
         >
-          Learn React
-        </a>
-      </header>
+          =
+        </button>
+      </div>
     </div>
   );
 }
 
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 export default App;
